76831d23b1802fabe50501caa16c596cd87c4ed9
rajarshimaitra <rajarshi149@gmail.com>
2024-04-28 23:04:21 +0530

    fix test

--- test.sh
+++ test.sh
@@ -9,6 +9,7 @@ chmod +x ./bash/run-bash.sh
 chmod +x ./python/run-python.sh
 chmod +x ./javascript/run-javascript.sh
 chmod +x ./rust/run-rust.sh
+chmod +x ./run.sh
 
 # Run the test scripts
 /bin/bash run.sh
