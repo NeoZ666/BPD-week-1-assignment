6d68ba8d76b664829d9ae222976ce1824cae4977
neoz666 <neoz.blockchain@gmail.com>
2024-05-30 06:14:54 +0000

    working till address

--- bash/solution.sh
+++ bash/solution.sh
@@ -9,19 +9,41 @@ rpc_call() {
   shift
   local params=$@
 
-  curl -s --user $RPC_USER:$RPC_PASSWORD --data-binary "{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", \"method\": \"$method\", \"params\": $params }" -H 'content-type: text/plain;' http://$RPC_HOST/
+  curl -s --user $RPC_USER:$RPC_PASSWORD --data-binary "{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", \"method\": \"$method\", \"params\": [$params] }" -H 'content-type: text/plain;' http://$RPC_HOST/
+}
+
+# Helper function to make RPC calls
+rpc_call2() {
+  local wallet=$1
+  local method=$2
+  shift 2
+  local params=$@
+
+  curl -s --user $RPC_USER:$RPC_PASSWORD --data-binary "{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", \"method\": \"$method\", \"params\": [$params] }" -H 'content-type: text/plain;' http://$RPC_HOST/wallet/$wallet
 }
 
 # Check Connection
-info=$(rpc_call "getblockchaininfo" "[\"\"]")
+info=$(rpc_call "getblockchaininfo")
 echo $info
 
 # Create and load wallet
+wallet=$(rpc_call "createwallet" \"walletName2\")
+echo $wallet
+
+unloaded=$(rpc_call "unloadwallet" "$wallet")
+echo $unloaded
 
 # Generate a new address
+address=$(rpc_call2 "walletName2" "getnewaddress") 
+echo $address
 
 # Mine 103 blocks to the new address
+mine=$(rpc_call "generatetoaddress" "[103, \"$address\"]")
+echo $mine
 
-# Send the transaction
+# # Send the transaction
+# txid=$(rpc_call "sendtoaddress" "[\"recipient_address\", amount]")
+# echo $txid
 
-# Output the transaction ID to a file
+# # Output the transaction ID to a file
+# echo $txid > out.txt
